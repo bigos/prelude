1907444c1579549b06c4e9fbaf4c8df976f7e9e6
Jacek Podkanski <ruby.object@googlemail.com>
2017-06-01 22:39:49 +0100

    added capability to use org files for Emacs configuration

--- init.el
+++ init.el
@@ -131,7 +131,11 @@ by Prelude.")
 ;; load the personal settings (this includes `custom-file')
 (when (file-exists-p prelude-personal-dir)
   (message "Loading personal configuration files in %s..." prelude-personal-dir)
-  (mapc 'load (directory-files prelude-personal-dir 't "^[^#\.].*el$")))
+  (mapc 'load (directory-files prelude-personal-dir 't "^[^#\.].*el$"))
+  (message "Loading org-mode personal configuration files in %s..." prelude-personal-dir)
+  (require 'org)
+  (mapc 'org-babel-load-file
+        (directory-files prelude-personal-dir 't "^[^#\.].*org$")))
 
 (message "Prelude is ready to do thy bidding, Master %s!" current-user)
 
