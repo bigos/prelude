e047f483226542da7f31665cae12b6d5a233b222
Jacek Podkanski <Jacek.Podkanski@conceptlifesciences.com>
2021-08-20 12:40:16 +0100

    trying org roam again

--- personal/organised.el
+++ personal/organised.el
@@ -228,6 +228,22 @@
    'org-babel-load-languages
    '((restclient . t)))
 
+(use-package org-roam
+  :ensure t
+  :custom
+  (org-roam-directory (file-truename "~/Documents/org-roam/"))
+  :bind (("C-c n l" . org-roam-buffer-toggle)
+         ("C-c n f" . org-roam-node-find)
+         ("C-c n g" . org-roam-graph)
+         ("C-c n i" . org-roam-node-insert)
+         ("C-c n c" . org-roam-capture)
+         ;; Dailies
+         ("C-c n j" . org-roam-dailies-capture-today))
+  :config
+  (org-roam-db-autosync-mode)
+  ;; If using org-roam-protocol
+  (require 'org-roam-protocol)
+
   (defun open-buffer-in-vscode ()
     (interactive)
 
