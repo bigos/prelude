c7ce1f4d67098559eee0f71778e37f720d7ddf4f
Jacek Podkanski <ruby.object@googlemail.com>
2022-12-02 13:47:08 +0000

    slime sly fix

--- personal/organised.el
+++ personal/organised.el
@@ -544,7 +544,9 @@
       (require 'redshank-loader)
       (eval-after-load "redshank-loader"
         `(redshank-setup '(lisp-mode-hook
-                           slime-repl-mode-hook)
+                           ; slime-repl-mode-hook
+                           sly-mrepl-mode-hook
+                           )
                          t))
 
 (defun unfold-lisp ()
