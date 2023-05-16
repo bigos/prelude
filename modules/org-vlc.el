0ac075b9ae4fb9e9d8ed1d4eb42bffecb3c19b95
Jacek Podkanski <Jacek.Podkanski@conceptlifesciences.com>
2023-05-16 12:48:26 +0100

    beginning of the new package

---
+++ modules/org-vlc.el
@@ -0,0 +1,33 @@
+;;; org-vlc.el --- a simple package                     -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2023 Jacek Podkanski
+
+;; Author: Jacek Podkanski
+;; Keywords: lisp
+;; Version: 0.0.1
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Org-vlc provides links for org mode file that allow to use vlc to open video
+;; files with the optional start time and stop time.
+
+;;; Code:
+
+;; code goes here
+(require 'org)
+
+(provide 'org-vlc)
+;;; org-vlc.el ends here
