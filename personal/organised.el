79f3d8e8b31d5739ac1eea3205eafd62c0dce139
Jacek Podkanski <ruby.object@googlemail.com>
2024-03-18 20:51:52 +0000

    experimental c# config

--- personal/organised.el
+++ personal/organised.el
@@ -457,6 +457,35 @@
 
 ;; this may not be needed because prelude defaults
 
+;;; *** C#
+
+(defun my-csharp-mode-setup ()
+  (omnisharp-mode)
+  (company-mode)
+  (flycheck-mode)
+
+  (setq indent-tabs-mode nil)
+  (setq c-syntactic-indentation t)
+  (c-set-style "ellemtel")
+  (setq c-basic-offset 4)
+  (setq truncate-lines t)
+  (setq tab-width 4)
+  (setq evil-shift-width 4)
+
+                                        ;csharp-mode README.md recommends this too
+                                        ;(electric-pair-mode 1)       ;; Emacs 24
+                                        ;(electric-pair-local-mode 1) ;; Emacs 25
+
+  (local-set-key (kbd "C-c r r") 'omnisharp-run-code-action-refactoring)
+  (local-set-key (kbd "C-c C-c") 'recompile))
+
+(use-package omnisharp
+  :ensure t
+  :config
+  (with-eval-after-load 'company
+    '(add-to-list 'company-backends #'company-omnisharp))
+  (add-hook 'csharp-mode-hook 'my-csharp-mode-setup))
+
 ;;; *** Ocaml
 ;; https://discuss.ocaml.org/t/how-to-start-with-emacs-to-work-on-ocaml-codebases/10312/19
 
