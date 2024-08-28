5cf5ed67ebfc04e4a643ad677802f35c4b88e823
Jacek Podkanski <ruby.object@googlemail.com>
2024-08-28 23:29:47 +0100

    removed org-brain

--- personal/organised.el
+++ personal/organised.el
@@ -440,32 +440,6 @@ Handles both Org-roam nodes, and string nodes (e.g. urls)."
 (require 'org-protocol)
 (require 'org-roam-protocol)
 
-(use-package org-brain
-  :ensure t
-  :init
-  (setq org-brain-path
-        "~/Documents/org-brain")
-  ;; For Evil users
-  (with-eval-after-load 'evil
-    (evil-set-initial-state 'org-brain-visualize-mode 'emacs))
-  :config
-  (bind-key "C-z b" 'org-brain-prefix-map org-mode-map)
-  (setq org-id-track-globally t)
-  (setq org-id-locations-file "~/.emacs.d/.org-id-locations")
-  (add-hook 'before-save-hook #'org-brain-ensure-ids-in-buffer)
-  (push '("b" "Brain" plain (function org-brain-goto-end)
-          "* %i%?" :empty-lines 1)
-        org-capture-templates)
-  (setq org-brain-visualize-default-choices 'all)
-  (setq org-brain-title-max-length 12)
-  (setq org-brain-include-file-entries nil
-        org-brain-file-entries-use-title nil))
-
-;; Allows you to edit entries directly from org-brain-visualize
-(use-package polymode
-  :config
-  (add-hook 'org-brain-visualize-mode-hook #'org-brain-polymode))
-
 (use-package websocket
   :ensure t
   :after org-roam)
