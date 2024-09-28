7140b8a6722c03b39493455f2960695bac98f1d1
Jacek Podkanski <ruby.object@googlemail.com>
2024-09-28 12:44:42 +0100

    delete duplicate lines

--- personal/organised.el
+++ personal/organised.el
@@ -77,6 +77,7 @@ Handles both Org-roam nodes, and string nodes (e.g. urls)."
 (global-set-key (kbd "C-z S") 'sort-lines)
 ;;; fix annoying C-s in macros
 (global-set-key (kbd "C-z F") 'search-forward)
+(global-set-key (kbd "C-z X") 'delete-duplicate-lines)
 
 (global-set-key (kbd "C-z m") 'multi-vterm)
 (add-hook 'vterm-mode-hook
