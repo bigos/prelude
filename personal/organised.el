b67b36ff981f51b19657802ba696f7ead1259394
Jacek Podkanski <ruby.object@googlemail.com>
2024-09-29 22:19:07 +0100

    regex version

--- personal/organised.el
+++ personal/organised.el
@@ -812,22 +812,27 @@ Handles both Org-roam nodes, and string nodes (e.g. urls)."
 (require 'cl-lib)
 (defun search-nearest-space-or-bracket ()
   (interactive)
-  (let ((s0-point (point)))
-    (goto-char
-     (1-
-      (or
-       (car
-        (cl-sort
-         (cl-remove-if
-          #'null
-          (cl-mapcar
-           (lambda (s)
-             (goto-char s0-point)
-             (search-forward s nil t))
-           (list " " ")" "\n")))
-         #'<=))
-       (1+
-        s0-point))))))
+  ;; (let ((s0-point (point)))
+  ;;   (goto-char
+  ;;    (1-
+  ;;     (or
+  ;;      (car
+  ;;       (cl-sort
+  ;;        (cl-remove-if
+  ;;         #'null
+  ;;         (cl-mapcar
+  ;;          (lambda (s)
+  ;;            (goto-char s0-point)
+  ;;            (search-forward s nil t))
+  ;;          (list " " ")" "\n")))
+  ;;        #'<=))
+  ;;      (1+
+  ;;       s0-point)))))
+
+  (goto-char (1- (car (cl-sort (cl-remove-if #'null
+                                             (list (re-search-forward " \\|)\\|\n" nil t)
+                                                   (1+ (point))))
+                               #'<=)))))
 
 (global-set-key (kbd "C-z V") 'search-nearest-space-or-bracket)
 
