2b2d44950b2e95e06dcd97c55d74f6ad16f9692c
Jacek Podkanski <Jacek.Podkanski@sgs.com>
2024-10-10 14:24:48 +0100

    trying different theme selector

--- personal/organised.el
+++ personal/organised.el
@@ -70,8 +70,7 @@ Handles both Org-roam nodes, and string nodes (e.g. urls)."
 (global-set-key (kbd "C-z r") 'crux-recentf-find-file)
 (global-set-key (kbd "C-z SPC") 'fixup-whitespace)
 (global-set-key (kbd "C-z L") 'ef-themes-select-light)
-(global-set-key (kbd "C-<f7>") 'ef-themes-select-light)
-(global-set-key (kbd "M-<f7>") 'ef-themes-select)
+(global-set-key (kbd "M-<f7>") 'counsel-load-theme)
 (global-set-key (kbd "C-z D") 'ef-themes-select-dark)
 ;;; grep uses expected regular expression if you replace -e with -P
 (global-set-key (kbd "C-z g") 'grep-find)
